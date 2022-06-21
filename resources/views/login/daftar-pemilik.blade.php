c58f0e6c40acb84cdedde702e188d9d4e4b9e0fa
itsmematovic <itsmematkovic@outlook.com>
2022-06-21 17:29:40 +0700

    tambah landing page

---
+++ resources/views/login/daftar-pemilik.blade.php
@@ -0,0 +1,36 @@
+@extends('login.index')
+@section('isi')
+
+<div class="row login-field">
+  <div class="col mt-auto">
+    <img src="{{url('images/graphics/daftar_p.png/')}}" width="90%" alt="">
+  </div>
+  <div class="col px-auto my-auto">
+    <a href="{{url('/landing')}}" class="text-muted " style="text-decoration: none">&lt;kembali</a>
+    <h3 class="judul text-center">Daftar Pemilik Kos</h3>
+    <form>
+      <div class="mb-3">
+        <label for="nama" class="form-label">Nama Lengkap</label>
+        <input type="text" class="form-control" id="nama" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="Email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="Email" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="noHp" class="form-label">Nomor Handphone</label>
+        <input type="number" class="form-control" id="noHp">
+      </div>
+      <div class="mb-3">
+        <label for="Password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="Password">
+      </div>
+      <div class="d-grid gap-2 col-12 mx-auto">
+        <a href="" type="button" class="btn btn-success">Daftar</a>
+      </div>
+    </form>
+    <small class="d-block text-center mt-3">Sudah memiliki akun? <a href="{{url('/masuk-pemilik')}}">login</a></small>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/login/login-pemilik.blade.php
@@ -0,0 +1,28 @@
+@extends('login.index')
+@section('isi')
+
+<div class="row login-field">
+  <div class="col mt-auto">
+    <img src="{{url('images/graphics/login_p.png/')}}" width="70%" alt="">
+  </div>
+  <div class="col px-auto my-auto">
+    <a href="{{url('/landing')}}" class="text-muted " style="text-decoration: none">&lt;kembali</a>
+    <h3 class="judul text-center">Login Pemilik Kos</h3>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="d-grid gap-2 col-12 mx-auto">
+        <a hreaf="" type="button" class="btn btn-success">Masuk</a>
+      </div>
+    </form>
+    <small class="d-block text-center mt-3">Belum Memililiki Akun? <a href="{{url('/daftar-pemilik')}}">Registrasi Disini</a></small>
+  </div>
+</div>
+
+@endsection
