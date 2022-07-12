b707e67cb29d450c67552daa7c2d175c9b0d08f1
Rian Maulana <43876647+rianmau-lana@users.noreply.github.com>
2022-07-12 13:25:30 +0700

    peraturan kost dan search kost

--- resources/views/layout/search.blade.php
+++ resources/views/layout/search.blade.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="card card-body shadow-blur mt-n5">
             <div class="container">
-                <div class="row text-center my-auto">
-                    <div class="col my-auto">
-                        <img src="{{url('images/icons/mdi_google-maps.png')}}" height="50px">
-                    </div>
-                    <form action="{{url('/search')}}" method="get">
+                <form action="{{url('/search')}}" method="get">
+                    <div class="row text-center my-auto">
+                        <div class="col my-auto">
+                            <img src="{{url('images/icons/mdi_google-maps.png')}}" height="50px">
+                        </div>
                         <div class="col-md-9 my-auto">
                             <div class="input-group my-auto ">
                                 <input type="text" name="keyword" class="form-control" placeholder="Ketik Lokasi atau Nama Kos">
@@ -15,8 +15,8 @@
                         <div class="col-md-2 my-auto">
                             <button type="submit" class="btn btn-success btn-lg my-auto">Cari</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
         
             </div>
         </div>
